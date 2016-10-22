efa46e5da3de0fe99bfb1f6c06ee8a033904b0b3
fothergilld <fothergilld@gmail.com>
2016-10-22 19:35:35 +0100

    add app for lists and failing unit test

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -14,6 +14,7 @@ class NewVisitorTest(unittest.TestCase):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
+        self.browser.implicitly_wait(3)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
