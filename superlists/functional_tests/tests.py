1f362a0d55c573dcd19d48fefa3fc32cc7705c1a
fothergilld <fothergilld@gmail.com>
2016-10-27 08:22:05 +0100

    added unique list urls and related tests

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -56,7 +56,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(4)
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
